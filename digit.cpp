e1839333def210d722ea64f22fd2c3fc7ebf681e
athulyathu <36473847+athulyathu@users.noreply.github.com>
2018-02-15 16:13:11 +0530

    digit.cpp

---
+++ digit.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+int no,a=0;
+cout<<"Enter any num : ";
+cin>>no;
+while(no>0)
+{
+no=no/10;
+a++;
+}
+cout<<"\n no. of digits in given number is: "<<a;
+reteun0;
+}
