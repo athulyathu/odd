74d46fcc61016cd9ed7ef2b9494fad3758317282
athulyathu <36473847+athulyathu@users.noreply.github.com>
2018-02-15 15:24:11 +0530

    natural.cpp

---
+++ natural.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int n,i,sum=0;
+	cout<<"enter the natural number";
+	cin>>n;
+	for(i=1;i<=1;i++){
+	sum+=i;
+}
+	cout<<"sum="<<sum;
+return 0;
+}
